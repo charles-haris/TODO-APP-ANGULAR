059d7f4fa9441259324a0cbb09b78997078305b7
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2024-07-04 15:37:09 +0000

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//FormsModule impoorted to allow the treatment of forms in Angular ,and FormsModule added in the array imports at the line 16
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
